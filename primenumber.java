15ad007c5245f2e32b98aebed27f5d313d97f54d
ayogitha <eliteresourcenetwork@gmail.com>
2025-01-30 22:35:25 +0530

    Add files via upload

---
+++ primenumber.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class primenumber {
+    public static boolean isPrime(int number) {
+        if (number <= 1) return false;
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);       
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        System.out.println(isPrime(number) ? number + " is prime." : number + " is not prime.");
+        scanner.close();
+    }
+
+    
+}
